4b7b7415ff0b395a9840d1e21fa02a02b0400344
David Sousa <david01sousa@hotmail.com>
2023-09-12 11:45:26 -0300

    chore: setup do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import { Button } from "./components/ui/button";
 
 export function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Button>Teste</Button>
     </>
   );
 }
